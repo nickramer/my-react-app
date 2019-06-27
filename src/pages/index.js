9f8c4e86b0a8ce8884007d20abbc7f309ec5ac40
Nicholas Kramer <nicholaskramer@Nicholass-MacBook-Pro.local>
2019-06-27 19:20:26 -0400

    push 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code react apps</h1>
-    <p>Complete courses and some stuff.</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code react apps</h1>
+        <p>Complete courses and some stuff.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </div>
 )
 
